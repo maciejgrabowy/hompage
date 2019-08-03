def493ace07ad6e47c140f091a11e891c705e9a8
Maciej Grabowy <macgrabowy@gmail.com>
2019-08-03 14:57:24 +0200

    adjusted hamburger menu

--- main.js
+++ main.js
@@ -79,3 +79,4 @@ navigationSwitcher.addEventListener('click', (e) => {
   const navigationList = document.querySelector('.navigation__list--js');
   navigationList.classList.toggle('navigation__list--visible')
 })
+
